b44da368f179ec997eba110067f00128714029f8
Gren CISSE Al-Karid <grencisse@gmail.com>
2023-03-25 12:21:59 +0000

    Update processing service

--- services/processing.py
+++ services/processing.py
@@ -1,4 +1,6 @@
 from services.namespace import Namespace
+from services.links import base_link
+from services.date import compute_word_days_in_month
 
 def get_direction_name(p):
     p = Namespace(p)
@@ -13,3 +15,24 @@ def get_direction_name(p):
         if div is not None:
             return div["shortName"]
         else: return p.subdivision["shortName"]
+
+def count_late_occurence(minuteDiffs:list):
+    occurences = [1 for t in minuteDiffs if t<0]
+    return len(occurences)
+
+def count_no_log_occurence(nb_log:int, date_string:str):
+    n_work_days = compute_word_days_in_month(date_string)
+    return n_work_days - nb_log
+
+def generate_daterange_link(link, positionId:int, startDate:str, endDate:str):
+    """
+    Args:
+        link (links_ns): A Namespace object created on Link dictionary.
+    Returns:
+        str: A string representing a full link. 
+        i.e: http://172.28.48.1:8080/api/v1/absence/{{s}}/{{e}}.
+    """
+    return base_link + link.prange + str(positionId) + "/" + startDate + "/" + endDate
+
+def generate_position_req_link(link, p):
+    return base_link + link.position + str(p)
