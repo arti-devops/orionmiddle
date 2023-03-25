44776be71e6f3c2ddedcd0dffa88879e56000f48
Gren CISSE Al-Karid <grencisse@gmail.com>
2023-03-25 12:23:12 +0000

    Upadate employee interface

--- interfaces/employee.py
+++ interfaces/employee.py
@@ -1,6 +1,7 @@
 import requests
+from services.date import *
 from services.links import *
-from services.processing import get_direction_name
+from services.processing import *
 
 def get_employee_list_bio_data():
     emp_data_list = list()
@@ -15,3 +16,32 @@ def get_employee_list_bio_data():
         emp_data["direction"] = get_direction_name(p)
         emp_data_list.append(emp_data)
     return emp_data_list
+
+def get_employee_list_checkin_data(emp_list:list, start_date:str, end_date:str):
+    log_data_list = []
+    for emp in emp_list:
+
+        logd = dict()
+
+        e = Namespace(emp)
+        pid = e.position_id
+
+        lc = generate_daterange_link(links_ns.logbook, pid, start_date, end_date)
+        la = generate_daterange_link(links_ns.absence, pid, start_date, end_date)
+
+        reqa = requests.get(la)
+        reqc = requests.get(lc)
+
+        loga = reqa.json()
+        logc = reqc.json()
+
+        #logd["position_id"] = pid
+        logd["checkins"] = len(logc)
+        logd["no_checkins"] = count_no_log_occurence(len(logc), start_date)
+        logd["late_arrivals"] = count_late_occurence([compute_time_diff_in_minutes(c["checkinTime"]) for c in logc])
+        logd["absences"] = len(loga)
+        logd["abs_justified"] = len([1 for a in loga if a["justified"]=="yes"])
+
+        log_data_list.append(logd)
+        emp["logs"] = logd
+    return emp_list, log_data_list
